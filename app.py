f4c2956ee53dbfada578ad1f2cf2b5ee039bb475
someone735 <smeone83@gmail.com>
2025-08-20 22:52:53 -0600

    resolved initial get response and developing submission

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from typing import List
 
@@ -13,13 +13,17 @@ SalesOrderCounter = 0
 
 def SalesOrderVerification(CustomerName: str, LineItemName: List[str], LineItemQuantity: List[int]):
     if not CustomerName or not LineItemName or not LineItemQuantity:
-        return False
+        return "Customer name, line item names, and quantities cannot be empty."
     if len(LineItemName) != len(LineItemQuantity):
         return "Line item names and quantities must match in length."
     for index, quantity in enumerate(LineItemQuantity):
         if quantity <= 0:
-            return f"{LineItemName[index]}\'s quantity must be greater than zero."
+            return f"The item {LineItemName[index]} quantity must be greater than zero."
     return True
+
+@app.get("/", response_class=HTMLResponse)
+async def serve_index():
+    return FileResponse("Template_Folder/index.html")
 
 @app.post("/order", response_class = HTMLResponse)
 async def submit(
@@ -27,18 +31,27 @@ async def submit(
     LineItemName: List[str] = Form(...),
     LineItemQuantity: List[int] = Form(...)
 ):
-    print("Customer Name:", CustomerName)
-    print("Line Item Names:", LineItemName)
-    print("Line Item Quantities:", LineItemQuantity)
-    if SalesOrderVerification(CustomerName, LineItemName, LineItemQuantity):
+    global SalesOrderCounter
+    
+    VerificationResponse = SalesOrderVerification(CustomerName, LineItemName, LineItemQuantity)
+    
+    if isinstance(VerificationResponse, bool):
+        SalesOrderCounter += 1
         OrdersDB[SalesOrderCounter] = {
             "CustomerName": CustomerName,
             "LineItemName": LineItemName,
             "LineItemQuantity": LineItemQuantity
         }
-        SalesOrderCounter += 1
-        return {"message": "Recieved:"} 
+        return f''' 
+        <div>Order received successfully!</div><br>
+        <div>Order ID: {SalesOrderCounter}</div>
+        <div>Customer Name: {CustomerName}</div>
+        <>
+        <div>Line Item Names: {LineItemName}</div>
+        <div>Line Item Quantities: {LineItemQuantity}</div> 
+        '''
+    
     else:
-        raise HTTPException(status_code=400, detail="Invalid order data")
+        raise HTTPException(status_code=400, detail = VerificationResponse)
     
 
